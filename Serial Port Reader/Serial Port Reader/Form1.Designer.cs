d2f9a65a3bf5811c2713e59ec2c2ddc2ddd053c0
Dağhan Güven <moderator0180@gmail.com>
2024-09-08 14:58:03 +0300

    Add files via upload

---
+++ Serial Port Reader/Serial Port Reader/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Serial_Port_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(23, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(124, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 111);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(542, 327);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(153, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "START";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "230400",
+            "460800",
+            "921600"});
+            this.comboBox2.Location = new System.Drawing.Point(148, 28);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(124, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(566, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Serial Port Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Serial Port Reader/Serial Port Reader/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serial_Port_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private SerialPort serialPort;
+        private string currentPort = "";
+        private string lastPort = "";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            comboBox2.Text = "9600";
+        }
+        private ArrayList PortNames = new ArrayList();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button1.Text = "Refreshing...";
+
+            comboBox1.Items.Clear();
+
+            PortNames = ArrayList.Adapter(SerialPort.GetPortNames());
+
+            for (int j = 0; j < PortNames.Count; j++)
+            {
+                comboBox1.Items.Add(PortNames[j]);
+            }
+
+            PortNames = new ArrayList();
+
+            button1.Enabled = true;
+            button1.Text = "Refresh";
+            comboBox1.Text = comboBox1.Items.Contains(lastPort) ? lastPort : comboBox1.Items.Count == 0 ? "" : comboBox1.Items[0].ToString();
+        }
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            await ReadFromSerialPortAsync(currentPort);
+        }
+
+        private async Task ReadFromSerialPortAsync(string portName)
+        {
+            serialPort = new SerialPort(portName, baudRate: Convert.ToInt32(comboBox2.Text));
+            serialPort.ReadTimeout = 5000;
+
+            try
+            {
+                serialPort.Open();
+                string result = await ReadLineAsync(serialPort);
+                richTextBox1.Text = result + "\n"+richTextBox1.Text;
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text = portName + ": " + ex.Message+"\n"+richTextBox1.Text;
+            }
+            finally
+            {
+                serialPort.Close();
+            }
+        }
+
+        private async Task<string> ReadLineAsync(SerialPort serialPort)
+        {
+            using (StreamReader reader = new StreamReader(serialPort.BaseStream))
+            {
+                try
+                {
+                    return await reader.ReadLineAsync();
+                }
+                catch (TimeoutException)
+                {
+                    return null;
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "")
+            {
+                currentPort = comboBox1.Text;
+                button3.Text = "STOP";
+                button3.BackColor = System.Drawing.Color.FromArgb(192, 0, 0);
+                button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+                button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(255, 128, 128);
+                button3.Click += button3___Click;
+                button3.Click -= button3_Click;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please select a port.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3___Click(object sender, EventArgs e)
+        {
+            button3.Text = "START";
+            button3.BackColor = System.Drawing.Color.FromArgb(0, 192, 0);
+            button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(128, 255, 128);
+            button3.Click += button3_Click;
+            button3.Click -= button3___Click;
+            timer1.Stop();
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            lastPort = comboBox1.Text == "" ? lastPort : comboBox1.Text;
+        }
+    }
+}
